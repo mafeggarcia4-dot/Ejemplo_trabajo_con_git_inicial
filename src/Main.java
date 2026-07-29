4a80c68ca50261692aee2d5b5514b29378a90360
mafegarcia <magarcia@cesde.net>
2026-07-28 20:32:05 -0500

    actualización main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Menu");
+        System.out.println("Actualización de commit");
     }
 }
